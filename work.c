8e5cd302095af2ea1b623b5a7d81f86715ac8a9e
stannyst <drahlovaleksandr@gmail.com>
2024-09-24 18:50:11 +0300

    added variables

--- work.c
+++ work.c
@@ -2,10 +2,11 @@
 
 int main() {
     int a = 2; 
-    int c, d;
+    int c, d, e, f;
     int b = 3; 
-    scanf("%d %d", &c, &d);
+    scanf("%d %d %d %d", &c, &d, &f, &e);
     printf("%d\n", a + b); 
     printf("%d\n", c + d); 
+    printf("%d\n", e + f); 
     return 0;
 }
